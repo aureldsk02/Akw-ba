b2fb6e7d61b9999c4415ef12b686b4c2cbcadc89
andysags13 <andy.sagbo@epitech.eu>
2025-06-20 10:24:47 +0100

    all

--- akweba/routes/api.php
+++ akweba/routes/api.php
@@ -19,5 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/businesses', [BusinessController::class, 'store']);
-Route::post('/businesses/{id}/generate-site', [BusinessController::class, 'generateSite']);
+Route::post('/businesses', [BusinessController::class, 'store']); 
